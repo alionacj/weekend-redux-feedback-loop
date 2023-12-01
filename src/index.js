f4bc15d14f33c631cdf730a6e646f5ce24123384
Leo Johnson <leochinonejohnson@gmail.com>
2023-12-01 15:56:06 -0600

    moved store to separate file

--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
 import React from 'react';
-import { createStore, combineReducers, applyMiddleware } from 'redux'
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { logger } from 'redux-logger';
 
+import store from './redux/store';
 import App from './components/App/App';
 
 import './index.css';
-
-const feedback = (state = {
-        feeling: 0,
-        understanding: 0,
-        support: 0,
-        comments: ''
-        }, action) => {
-    const payload = action.payload
-    switch (action.type) {
-        case 'FEELING_SCORE':
-            return {...state, feeling: payload}
-            break;
-        default:
-            return state
-            break;
-    }
-}
-
-const store = createStore(
-    combineReducers({
-        feedback
-    }),
-    applyMiddleware(logger),
-)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
