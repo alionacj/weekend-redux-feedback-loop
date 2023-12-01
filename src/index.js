9e8eac6a8197cc9ef7aee7af9e30287cbe52fd77
Leo Johnson <leochinonejohnson@gmail.com>
2023-12-01 14:47:20 -0600

    established reducer as object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider, logger } from 'react-redux'
+
 import App from './components/App/App';
+
+import './index.css';
+
+const feedback = (state={}, action) => {
+    const payload = action.payload
+    switch (action.type) {
+        case 'DISPATCH_MESSAGE':
+        
+            break;
+        default:
+            return state
+            break;
+    }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
